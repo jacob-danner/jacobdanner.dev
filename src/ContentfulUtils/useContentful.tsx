60a363fcf9ba5c025063945cd83d1e6519574ce4
Jacob Danner <jacob.danner@drake.edu>
2023-01-23 23:18:08 -0600

    added category select filter to AllBlogs

--- src/ContentfulUtils/useContentful.tsx
+++ src/ContentfulUtils/useContentful.tsx
@@ -7,6 +7,7 @@ interface Blog {
         date: string
         slug: string
         markdownContent: string
+        category: string[]
     }
 }
 
--- src/Pages/AllBlogs.tsx
+++ src/Pages/AllBlogs.tsx
@@ -1,26 +1,47 @@
+import { useState } from "react";
 import { useBlogs } from "../ContentfulUtils/useContentful";
 import { BlogCard } from "./AllBlogs/BlogCard";
 
+const CATEGORIES = [
+  'All', 'Machine Learning', 'Python', 'Natural Language', 'Other'
+]
+
 const AllBlogs: React.FC = () => {
   const [blogs, isLoading] = useBlogs();
+  const [filterWord, setFilterWord] = useState('All')
 
-  const buildHTML = () => {
-    if (isLoading) return <p className="LoadingScreen">Loading...</p>
+  if (isLoading) return <p className="LoadingScreen">Loading...</p>
 
-    return (
-      <div className="BlogsWrapper">
-        {blogs!.map(el => {
-          return <BlogCard title={el.fields.title} date={el.fields.date} slug={el.fields.slug} key={el.fields.slug} />
-        })}
-      </div>
-    )
-  }
+  const filtered = blogs?.filter(el => {
+    if(el.fields && el.fields.category) {
+      const cateogories: string[] = el.fields.category
+      if (filterWord !== 'All')
+        return cateogories.includes(filterWord)
+      else return true
+    }
+    else {
+      return false
+    }
+  })
 
   return (
     <>
-      {buildHTML()}
+      <select onChange={e => setFilterWord(e.currentTarget.value)}>
+        {CATEGORIES.map((cat: string, index: number) =>( 
+          <option key={index} value={cat}>
+          {cat}
+        </option>
+        ))}
+      </select>
+
+      <div >
+        {filtered!.map(el => {
+          return <BlogCard title={el.fields.title} date={el.fields.date} slug={el.fields.slug} key={el.fields.slug} />
+        })}
+      </div>
     </>
   )
 }
+
 
 export { AllBlogs }
